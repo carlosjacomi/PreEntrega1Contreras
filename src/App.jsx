6268dcf32cc3f7c38e8bb85ca9783e352a590663
Carlos Contreras <30361724+carlosjacomi@users.noreply.github.com>
2023-03-04 17:15:35 -0300

    Feature - Creación del Componente ItemCount.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar'
 import ListContainer from './components/ListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar/>
       <ListContainer/>
+      <ItemCount/>
     </>
   )
 }
